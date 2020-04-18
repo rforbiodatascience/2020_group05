887edfd8bdd7ee8fc0837cf4ba90c71f8856201c
cartref <s193122@student.dtu.dk>
2020-04-18 13:16:43 +0000

    doit

---
+++ test/00_doit.R
@@ -0,0 +1,7 @@
+# Run all scripts
+# ------------------------------------------------------------------------------
+setwd('/cloud/project/week6_github/project_root')
+source(file = "R/01_load.R")
+source(file = "R/02_clean.R")
+source(file = "R/03_augment.R")
+source(file = "R/04_analysis_i.R")
