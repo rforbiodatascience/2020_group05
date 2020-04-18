a342a8af6b1c45086123d5b3103d090334cbd626
asaksager <astridsaksager@gmail.com>
2020-04-18 13:40:45 +0000

    Download data fra package

--- R/01_load.R
+++ R/01_load.R
@@ -1,2 +1,6 @@
 #This script will load the data from either the package or from Raw depending.. 
 
+library(devtools)
+install_github('ramhiser/datamicroarray')
+data('khan', package = 'datamicroarray')
+View(khan$x)
