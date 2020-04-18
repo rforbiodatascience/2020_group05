a94ca47f1675b800e7e31f44871d0916ed95de8f
ceci4323 <cillethystrup@live.dk>
2020-04-18 13:15:43 +0000

    added proj_func script

---
+++ R/99_proj_func.R
@@ -0,0 +1,10 @@
+# Define project functions
+# ------------------------------------------------------------------------------
+func1 <- function(x){
+  return(2*x)
+}
+
+func2 <- function(x){
+  return(x^2)
+}
+...
--- R/lort.R
+++
@@ -1,1 +0,0 @@
-lorte lort
