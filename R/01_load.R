02dab6a9e0d363e48a63d1f9d7f68812368298f5
cartref <s193122@student.dtu.dk>
2020-04-20 06:59:36 +0000

    update

--- R/01_load.R
+++ R/01_load.R
@@ -1,6 +1,4 @@
 #This script will load the data from either the package or from Raw depending.. 
 
-library(devtools)
-install_github('ramhiser/datamicroarray')
-data('khan', package = 'datamicroarray')
-View(khan$x)
+library(readxl)
+df <- read.csv('/cloud/project/_raw/dmd.csv')
