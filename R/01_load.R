431201f4cc2e0fbbee985cc046fb077bb3544c96
asaksager <astridsaksager@gmail.com>
2020-04-21 12:11:30 +0000

    splitting

--- R/01_load.R
+++ R/01_load.R
@@ -1,4 +1,18 @@
 #This script will load the data from either the package or from Raw depending.. 
 
 library(readxl)
-df <- read.csv('/cloud/project/_raw/dmd.csv')
+df <- read.csv(file = '/cloud/project/_raw/dmd.csv')
+df
+
+#To use tibble load tidyverse 
+library(tidyverse)
+filter(data = df, obsno== 1)
+df_split
+#---------------------Forget the following 
+#Seperating the table (raw) into two tables
+#We need to group the data in order to split it with dplyr
+df_grouped <- df %>% 
+  group_by(obsno)
+
+df_split <- group_split(df_grouped)
+df_split
