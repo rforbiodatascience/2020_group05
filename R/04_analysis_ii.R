e9d2314f0e05ea0d68af8f1e5baca9c6f60e8234
ceci4323 <cillethystrup@live.dk>
2020-04-27 06:42:25 +0000

    update

--- R/04_analysis_ii.R
+++ R/04_analysis_ii.R
@@ -7,6 +7,7 @@ rm(list = ls())
 # Load libraries
 # ------------------------------------------------------------------------------
 library("tidyverse")
+library("ggrepel")
 
 # Define functions
 # ------------------------------------------------------------------------------
@@ -30,8 +31,31 @@ pca_values <- drop_na(pca_values) %>%
   slice(1:20) %>%
   write_tsv(path = "data/04_PCA_table.tsv")
 
+#creation of PCA object
 pca_values <- pca_values %>% 
   prcomp(center = TRUE, scale = TRUE)
 
+#getting x and y values of the two PC components we want to plot
+x <- bl62_pca %>% 
+  tidy("pcs") %>% 
+  filter(PC==1) %>% 
+  pull(percent)
 
-View(pca_values)
+x <- str_c("PC1 (", round(x*100, 2), "%)")
+y <- bl62_pca %>%
+  tidy("pcs") %>% 
+  filter(PC==2) %>% 
+  pull(percent)
+
+y <- str_c("PC2 (", round(y*100, 2), "%)")
+
+# hhh
+
+#plotting the PCA
+bl62_pca_aug %>%
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2,
+             label = aa, colour = chem_class)) + 
+             geom_label_repel() + 
+             theme(legend.position = "bottom") +  
+             scale_colour_manual(values = c("red", "blue", "black", "purple", "green", "yellow")) +
+             labs(x = x, y = y)
