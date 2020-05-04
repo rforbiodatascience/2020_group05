842e22f440209631888caf4f23924b5481a39ac0
ceci4323 <cillethystrup@live.dk>
2020-05-04 08:17:20 +0000

    hej

--- R/linear_models.R
+++ R/linear_models.R
@@ -6,10 +6,37 @@ rm(list = ls())
 # Load libraries
 # ------------------------------------------------------------------------------
 library("tidyverse")
+library("broom")
+library("rsample")
+library("tidymodels")
+#install.packages("rsample")
+#install.packages("tidymodels")
 
 # Load data
 # ------------------------------------------------------------------------------
-df <- read_tsv(file = "data/01_df.tsv", col_names = TRUE)
-df_carrier <- read_tsv(file = "data/01_carrier_data.tsv")
+data <- read_tsv(file = "data/03_aug_data.tsv")
+View(data)
 
-# Wrangle data df
+# Wrangle data
+# ------------------------------------------------------------------------------
+# leave one out
+data_batch <- loo_cv(data)
+View(data_batch)
+#Modeling
+#-------------------------------------------------------------------------------
+#Simpel linear model - no subcategorising 
+simple_model <- lm(carrier ~ LD + H + PK + CK, data = data)
+log_reg_model <- glm(carrier ~ PK + LD + H + CK, 
+                     family = binomial(link = "logit"), 
+                     data = data)
+
+#coefficients for the models
+simple_model %>% tidy()
+# only intercept, LD and H 
+
+log_reg_model %>% tidy()
+# only intercept, H and CK
+
+# brug den her function på en eller anden maade
+predict(simple_model, newdata = data_batch())
+
